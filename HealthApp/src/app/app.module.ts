170d5bef81c92bec3be14ee74589de69d1d07439
rhyd0n <therealebraheem@gmail.com>
2023-09-04 18:11:12 -0700

    modified:   HealthApp/src/app/app.module.ts deleted:    HealthApp/src/app/components/main/home/pngtree-birthday-cake-candles-celebration-transparent-png-image_9116317.png

--- HealthApp/src/app/app.module.ts
+++ HealthApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/main/home/home.component';
+import { NavbarComponent } from './components/main/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
